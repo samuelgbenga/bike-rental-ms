d0cf99990d22af23997e0866ee6874964fad5218
samuelgbenga <samuelgbenga972@gmail.com>
2025-01-04 01:49:22 +0100

    bike rental movment

---
+++ core-api/src/main/java/io/axoniq/demo/bikerental/coreapi/App.java
@@ -0,0 +1,13 @@
+package io.axoniq.demo.bikerental.coreapi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
