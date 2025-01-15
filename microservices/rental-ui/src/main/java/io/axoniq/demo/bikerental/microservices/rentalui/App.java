23396b35b8a93b150656fd37a72cbeee9bc0370c
samuelgbenga <samuelgbenga972@gmail.com>
2025-01-15 13:46:24 +0100

    microservice evolution

---
+++ microservices/rental-ui/src/main/java/io/axoniq/demo/bikerental/microservices/rentalui/App.java
@@ -0,0 +1,13 @@
+package io.axoniq.demo.bikerental.microservices.rentalui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
