be9b6fa131ab58d5132af577d79616378f03c8be
samuelgbenga <samuelgbenga972@gmail.com>
2025-01-15 17:31:47 +0100

    new commit

--- create-microservices.sh
+++ create-microservices.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 echo "Copying Command component to Rental-Command"
-cp rental/src/main/java/io/axoniq/demo/bikerental/rental/command/*.java microservices/rental-command/src/main/java/io/axoniq/demo/bikerental/rental/command/
+cp rental/src/main/java/io/axoniq/demo/bikerental/rental/command/*.java microservices/rental-command/src/main/java/io/axoniq/demo/bikerental/microservices/rentalcommand
 
 echo "Copying Payment Saga component to Rental-Payment"
-cp rental/src/main/java/io/axoniq/demo/bikerental/rental/paymentsaga/*.java microservices/rental-payment/src/main/java/io/axoniq/demo/bikerental/rental/paymentsaga/
+cp rental/src/main/java/io/axoniq/demo/bikerental/rental/paymentsaga/*.java microservices/rental-payment/src/main/java/io/axoniq/demo/bikerental/microservices/rentalpayment
 
 echo "Copying Query component to Rental-Query"
-cp rental/src/main/java/io/axoniq/demo/bikerental/rental/query/*.java microservices/rental-query/src/main/java/io/axoniq/demo/bikerental/rental/query/
+cp rental/src/main/java/io/axoniq/demo/bikerental/rental/query/*.java microservices/rental-query/src/main/java/io/axoniq/demo/bikerental/microservices/rentalquery
 
 echo "Copying UI component to Rental-UI"
-cp rental/src/main/java/io/axoniq/demo/bikerental/rental/ui/*.java microservices/rental-ui/src/main/java/io/axoniq/demo/bikerental/rental/ui/
+cp rental/src/main/java/io/axoniq/demo/bikerental/rental/ui/*.java microservices/rental-ui/src/main/java/io/axoniq/demo/bikerental/microservices/rentalui
 
 echo "Finished microservices creation process"
---
+++ payment/src/main/java/io/axoniq/demo/bikerental/payment/payment/PaymentController.java
@@ -0,0 +1,53 @@
+package io.axoniq.demo.bikerental.payment.payment;
+
+import io.axoniq.demo.bikerental.coreapi.payment.ConfirmPaymentCommand;
+import io.axoniq.demo.bikerental.coreapi.payment.PaymentStatus;
+import io.axoniq.demo.bikerental.coreapi.payment.RejectPaymentCommand;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+public class PaymentController {
+
+    private final QueryGateway queryGateway;
+    private final CommandGateway commandGateway;
+
+    public PaymentController(QueryGateway queryGateway, CommandGateway commandGateway) {
+        this.queryGateway = queryGateway;
+        this.commandGateway = commandGateway;
+    }
+
+    @GetMapping("/status/{paymentId}")
+    public CompletableFuture<PaymentStatus> getStatus(@PathVariable("paymentId") String paymentId) {
+        return queryGateway.query("getStatus", paymentId, PaymentStatus.class);
+    }
+
+    @GetMapping("/findPayment")
+    public CompletableFuture<String> findPaymentId(@RequestParam("reference") String paymentReference) {
+        return queryGateway.query("getPaymentId", paymentReference, String.class);
+    }
+
+    @PostMapping("/acceptPayment")
+    public CompletableFuture<Void> confirmPayment(@RequestParam("id") String paymentId) {
+        return commandGateway.send(new ConfirmPaymentCommand(paymentId));
+    }
+
+    @PostMapping("/rejectPayment")
+    public CompletableFuture<Void> rejectPayment(@RequestParam("id") String paymentId) {
+        return commandGateway.send(new RejectPaymentCommand(paymentId));
+    }
+
+    @GetMapping("/status")
+    public Flux<PaymentStatus> getStatus(@RequestParam(value = "status", required = false) PaymentStatus.Status status) {
+        return Flux.from(queryGateway.streamingQuery("getAllPayments", status, PaymentStatus.class));
+    }
+
+}
